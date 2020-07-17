1650cf5beb5949bb1a55f197cabf5998622d7315
vkfbmzef <monkeylordjunior@gmail.com>
2020-07-17 16:09:40 -0700

    fixed null input bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -98,9 +98,6 @@ for (var i = 0; i < enemyInfo.length; i++) {
         // reset enemy.health before starting new fight
         pickedEnemyObj.health = randomNumber(40, 60);
 
-        // use debugger to pause script from running and check what's going on at that moment in the code
-        // debugger;
-
         // pass the pickedenemy.name variable's value into the fight function, where it will assume the value of the enemy.name parameter
         fight(pickedEnemyObj);
 
@@ -176,10 +173,22 @@ var shop = function() {
   }
 };
 
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+
+while (name === "" || name === null) {
+  name = prompt("What is your robot's name?");
+}
+
+  console.log("Your robot's name is " + name);
+  return name;
+};
+
 //Moved objects here to avoid error of enemyInfo array being defined before randonNumber
 // Object containing player information
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
